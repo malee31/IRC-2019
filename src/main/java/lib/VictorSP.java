9f1d9663140a6b41d060c26a6bd5c5eeb3c46d38
Rishi Nath <rishi.nath@gmail.com>
2019-09-14 18:13:51 -0700

    sneaky victor

--- src/main/java/lib/VictorSP.java
+++ src/main/java/lib/VictorSP.java
@@ -1,4 +1,16 @@
 package lib;
 
-public class VictorSP {
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class VictorSP extends TalonSRX {
+    public VictorSP(int id) {
+        super(id);
+
+        super.configFactoryDefault();
+    }
+
+    public void setSpeed(double speed) {
+        super.set(ControlMode.PercentOutput, speed);
+    }
 }
